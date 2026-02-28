def6c48ec954cebfec0fe62f1edb98a323bc4833
Andrei Soloviev <andreisolowiew872@yandex.ru>
2026-02-28 09:50:44 +0700

    response with 200 status-code to endpoint GET /integrations

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import * as swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const _app = await NestFactory.create(AppModule);
+
+  // Создание документа Swagger
+  const _swaggerDocument = YAML.load('docs/openapi.yaml');
+  // Создание интерфейса Swagger
+  _app.use('/docs', swaggerUi.serve, swaggerUi.setup(_swaggerDocument));
+
+  // Запуск приложения
+  await _app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
