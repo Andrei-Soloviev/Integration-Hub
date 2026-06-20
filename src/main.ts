bfe50201839e276ab9e3acb860572127c127e251
Andrei Soloviev <68671961+Andrei-Soloviev@users.noreply.github.com>
2026-06-20 14:52:22 +0700

    feat: add initial API specification (#3)
    - initialize API documentation structure
    - add Integration schemas and endpoints:
      - GET /integrations
      - GET /integrations/{id}
      - POST /integrations
      - PATCH /integrations/{id}
      - DELETE /integrations/{id}
    
    - add Event schemas and endpoints:
      - GET /events
      - GET /events/{id}
      - POST /events/{id}/retry
    
    - add common error schemas
    - add pagination support for list endpoints
    - refactor Integration and Event schemas
    - split OpenAPI specification into separate files
    - update README according to integration flow changes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+import SwaggerParser from '@apidevtools/swagger-parser';
 import { NestFactory } from '@nestjs/core';
+import * as path from 'path';
+import * as swaggerUi from 'swagger-ui-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const _app = await NestFactory.create(AppModule);
+
+  try {
+    // 1. Указываем путь к главному файлу OpenAPI
+    const openApiPath = path.resolve(process.cwd(), 'docs/openapi.yaml');
+    // 2. Собираем все файлы в один объект, разрешая все $ref ссылки
+    const swaggerDocument = await SwaggerParser.bundle(openApiPath);
+
+    // 3. Создание и настройка интерфейса Swagger UI
+    _app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  } catch (error) {
+    console.error('Failed to generate Swagger documentation', error);
+  }
+
+  const port = process.env.PORT || 3000;
+  await _app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
